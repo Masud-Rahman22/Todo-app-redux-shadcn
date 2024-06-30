811e24cdd653228283e73f74f7c679b7a765d340
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-07-01 04:08:14 +0600

    working on adding a todo

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,17 +1,24 @@
+import AddTodoModal from "./AddTodoModal";
+import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = ()=>{
     return (
         <div>
         <div className="flex justify-between mb-5 ">
-            <h1>AddTodoModal</h1>
-            <h1>TodoFilter</h1>
+            <AddTodoModal></AddTodoModal>
+            <TodoFilter></TodoFilter>
         </div>
         <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
-            <div className="bg-red-500 p-5 w-full h-full rounded-lg space-y-3">
+            <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+                <TodoCard></TodoCard>
+                <TodoCard></TodoCard>
+                <TodoCard></TodoCard>
+                <TodoCard></TodoCard>
+            </div>
             {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
             <p>There is no task pending</p>{' '}
           </div> */}            
-            </div>
         </div>
         </div>
     )
--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -1,11 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = {
-    todo: [],
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type TTodo = {
+    title: string,
+    description: string,
+    isCompleted: boolean
+}
+
+type TInitialState = {
+    todos: TTodo[]
+}
+
+const initialState: TInitialState = {
+    todos: [],
 }
 const todoSlice = createSlice({
     name: "todo",
     initialState,
-    reducers: {},
+    reducers: {
+        addTodo: (state, action: PayloadAction<TTodo>) => {
+            state.todos.push({ ...action.payload, isCompleted: false })
+        }
+    },
 })
 
 export default todoSlice.reducer;
